9f5cbcfa26796f27cc86ffd7711e3beea3d203cb
Stefano Garzarella <sgarzare@redhat.com>
2024-01-26 18:15:07 +0100

    start-kbs: add param to flush the DB
    Signed-off-by: Stefano Garzarella <sgarzare@redhat.com>

--- start-kbs.sh
+++ start-kbs.sh
@@ -5,16 +5,39 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 # Load VM configuration
 source "${SCRIPT_PATH}/vm.conf"
 
+FLUSH_DB=0
+
 function usage
 {
     echo -e "usage: $0 [OPTION...]"
     echo -e ""
     echo -e "Start Key Broker Server and SVSM proxy for QEMU"
     echo -e ""
+    echo -e " -f, --flush-db      flush the entire KBS database"
     echo -e " -h, --help          print this help"
 }
 
+while [ "$1" != "" ]; do
+    case $1 in
+        -f | --flush-db )
+            FLUSH_DB=1
+            ;;
+        -h | --help )
+            usage
+            exit
+            ;;
+        * )
+            echo -e "\nParameter not found: $1\n"
+            usage
+            exit 1
+    esac
+    shift
+done
+
 pushd "${SCRIPT_PATH}/kbs/reference-kbs"
+if [ "${FLUSH_DB}" == "1" ]; then
+    rm -f db/diesel/db.sqlite
+fi
 cargo run &
 popd
 
